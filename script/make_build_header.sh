9b0463b6deb345abe57c666b897d6ef71c4c0941
Coney Wu <coney.wu@gmail.com>
2014-02-07 22:39:29 +0800

    add my default kernel config.gz

--- script/make_build_header.sh
+++ script/make_build_header.sh
@@ -10,14 +10,16 @@ export ARCH=arm
 export CROSS_COMPILE=$CCPREFIX
 export INSTALL_MOD_PATH=$RASPI_BASE/temp/modules
 
-log Clean kernel source
-make mrproper -C $KERNEL_SRC
-
-log Decompress kernel config
+log Check kernel config
 if [[ ! -f $KERNEL_CONFIG ]]; then
     echo "Can't find $KERNEL_CONFIG, get it from /proc/config.gz in raspi"
     exit -1
 fi
+
+log Clean kernel source
+make mrproper -C $KERNEL_SRC
+
+log Decompress kernel config
 cat $KERNEL_CONFIG | gzip -d > $KERNEL_SRC/.config
 
 log Make kernel
