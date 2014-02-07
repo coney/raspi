2bc7efc16e15e6f1fc00c9eda4009462e88ca757
Coney Wu <coney.wu@gmail.com>
2014-02-07 23:55:38 +0800

    move CROSS_COMPILE and ARCH out of script

--- script/make_build_header.sh
+++ script/make_build_header.sh
@@ -4,11 +4,8 @@ function log {
     echo $*
 }
 
-CD=$(pwd)
 KERNEL_CONFIG=config.gz
-export ARCH=arm
-export CROSS_COMPILE=$CCPREFIX
-export INSTALL_MOD_PATH=$RASPI_BASE/temp/modules
+INSTALL_MOD_PATH=$RASPI_BASE/temp/modules
 
 log Check kernel config
 if [[ ! -f $KERNEL_CONFIG ]]; then
@@ -22,11 +19,17 @@ make mrproper -C $KERNEL_SRC
 log Decompress kernel config
 cat $KERNEL_CONFIG | gzip -d > $KERNEL_SRC/.config
 
+log Prepare for modules compilation
+make -C $KERNEL_SRC modules_prepare
+exit 0
+
 log Make kernel
 make -C $KERNEL_SRC
 
-log Install Modules
-mkdir -p $INSTALL_MOD_PATH
-make -C $KERNEL_SRC modules_install
+if [[ $1 -eq "install "]]; then
+	log Install Modules
+	mkdir -p $INSTALL_MOD_PATH
+	make -C $KERNEL_SRC modules_install
+fi
 
 
